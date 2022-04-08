ea63d4fc0e5f4fd174b0040e0957c83aad52771d
aizirekashymzhanova <96565067+aizirekashymzhanova@users.noreply.github.com>
2022-04-08 12:45:55 +0600

    navbar

---
+++ src/Components/Products/OneProduct/OneProduct.jsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Grid, IconButton } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useCart } from "../../../Context/CartContextProvider";
+import { Link } from "react-router-dom";
+
+export default function OneProduct({ item }) {
+  const { addDelToCart, isProdInCart } = useCart();
+  const [inCart, setInCart] = React.useState(isProdInCart(item.id));
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={item.img}
+          alt={item.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.title}
+          </Typography>
+          <Typography variant="h6" color="green">
+            ${item.price}
+          </Typography>
+          <Typography variant="body1">Type: {item.type}</Typography>
+          <Typography variant="body2" color="text.secondary">
+            {item.description}
+          </Typography>
+        </CardContent>
+
+        <CardActions>
+          <IconButton
+            onClick={() => {
+              addDelToCart(item);
+              setInCart(isProdInCart(item.id));
+            }}
+            color={inCart ? "secondary" : "inherit"}
+          >
+            <ShoppingCartIcon />
+          </IconButton>
+          <Button component={Link} to={`detail/${item.id}`} size="small">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
+}
--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
 
 const MainLayout = () => {
-  return <div></div>;
+  return (
+    <div
+      style={{ display: "flex", minHeight: "100vh", flexDirection: "column" }}
+    >
+      <Navbar />
+      <Outlet />
+      {/* <Footer /> */}
+    </div>
+  );
 };
 
 export default MainLayout;

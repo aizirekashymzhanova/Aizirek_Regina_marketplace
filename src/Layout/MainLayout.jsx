5b988d90d99204d84fa1f6ed885216e53af4673f
unknown <svtbkvnr@gmail.com>
2022-04-08 12:47:46 +0600

    nav

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
+import Navbar from "../Components/Navbar/Navbar";
 
 const MainLayout = () => {
-  return <div></div>;
+  return (
+    <div
+      style={{ display: "flex", minHeight: "100vh", flexDirection: "column" }}
+    >
+      <Navbar />
+      <Outlet />
+      {/* <Footer /> */}
+    </div>
+  );
 };
 
 export default MainLayout;

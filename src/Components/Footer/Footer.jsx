5a963bad8be286177e85b06e1482626dc11d5305
aizirekashymzhanova <96565067+aizirekashymzhanova@users.noreply.github.com>
2022-04-08 18:02:49 +0600

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,53 @@
+import {
+  Container,
+  IconButton,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <h2>Footer</h2>
+      <Container align="center">
+        <Box className="footer-inside">
+          <ListItemButton component={Link} to="/products">
+            <ListItemText primary="SHOP" />
+          </ListItemButton>
+          <ListItemButton component={Link} to="/about">
+            <ListItemText primary="ABOUT" />
+          </ListItemButton>
+          <ListItemButton component={Link} to="/products">
+            <ListItemText primary="CONTACT" />
+          </ListItemButton>
+        </Box>
+        <Box className="footer-inside" align="center">
+          <IconButton>
+            <InstagramIcon />
+          </IconButton>
+          <IconButton>
+            <FacebookIcon />
+          </IconButton>
+          <IconButton>
+            <PinterestIcon />
+          </IconButton>
+          <IconButton>
+            <TwitterIcon />
+          </IconButton>
+        </Box>
+        <Typography>
+          ©2022 Sweet Lady Jane. All Rights Reserved. | Website by A & R
+          Creative
+        </Typography>
+      </Container>
     </div>
   );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -49,6 +49,17 @@ const Home = () => {
         <Typography className="home-p" color="red" sx={{ fontWeight: "bold" }}>
           Our mission is simple – we make good things even better.
         </Typography>
+        <Container>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2923.7891933950223!2d74.58995091546709!3d42.87729341027228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389ec98281e85c63%3A0x305e5b2a69cb4fcb!2sMakers%20Studio!5e0!3m2!1sru!2skg!4v1649415754983!5m2!1sru!2skg"
+            width="100%"
+            height="300"
+            style={{ border: "0px" }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </Container>
       </Container>
     </>
   );
